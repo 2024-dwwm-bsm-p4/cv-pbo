f3f6d15a55d78100d6628c1a1d1223af129fb4ac
Pbourgeois62 <pbourgeois62200@gmail.com>
2024-09-10 15:38:13 +0200

    squelette HTML terminé

--- cv.php
+++ cv.php
@@ -1,13 +1,89 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="stylesheet" href="styles.css">
     <title>CV</title>
 </head>
+
 <body>
 
-<h1>Développeur web - web mobile full stack</h1>  
+<img src="docs/" alt="Photo de profil" width="" height="">
+
+    <h1>Développeur web - web mobile full stack</h1>
+
+    <div class="bloc">
+
+        <h2>Profil</h2>
+        <p>
+
+        <ul>
+            <li>En formation de développeur web / web-mobil full stack chez Simplon.</li>
+            <li>53 rue Ferdinand Buisson 62200 boulogne sur mer</li>
+            <li>0766591470</li>
+            <li>philippe.bourgeois62@laposte.net</li>
+        </ul>
+        </p>
+
+    </div>
+
+    <div class="bloc">
+
+        <h2>Competences</h2>
+        <p>
+
+        <ul><strong>
+                <li>Autonomie</li>
+                <li>Capacité de décision</li>
+                <li>Curiosité</li>
+                <li>Créactivité</li>
+                <li>Rigueur</li>
+                <li>Travail en équipe</li>
+        </ul></strong>
+        </p>
+
+    </div>
+
+    <div class="bloc">
+
+        <h2>Divers</h2>
+        <p>
+
+        <ul>
+            <li><strong>Anglais </strong>Intermédiaire</li>
+            <li><strong>Français </strong>Courant</li>
+            <li><strong>Permis: </strong>B- Véhicule léger</li>
+            <li><strong>Centre d'interet: </strong>Ebenisterie, musique, jardinage</li>
+        </ul>
+        </p>
+
+    </div>
+
+    <div class="bloc">
+        <h2>Formations</h2>
+        <p>
+
+        <ul>
+            <li><strong>2010: </strong>CAP de Menisier agenceur</li>
+            <li><strong>2008: </strong>CAP d'Ebeniste</li>
+            <li><strong>2005: </strong>BAC scientifique option maths</li>
+
+        </ul>
+        </p>
+    </div>
+
+    <div class="bloc">
+        <h2>Experiences</h2>
+        <p>
+            Dernière expérience(02/2020 - 05/2022) : <strong>Agent d'entretien des espaces naturels à Rivages propres.</strong> Experience m'ayant permis de travailler en complète autonomie et parfois avec la responsabilité d'une équipe.
+        </p>
+    </div>
+
+
+
+
 </body>
+
 </html
