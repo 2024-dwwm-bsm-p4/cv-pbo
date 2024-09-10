3c714ef8bbb8c0d5b85ff7f1c0bd1fcd0e976647
Pbourgeois62 <pbourgeois62200@gmail.com>
2024-09-10 20:42:32 +0200

    Affichage smartphone OK

--- cv.php
+++ cv.php
@@ -9,42 +9,63 @@
 </head>
 
 <body>
+    <div class="bloc">
 
-<img src="docs/" alt="Photo de profil" width="" height="">
+        <img src="docs/" alt="Photo de profil" width="" height="">
 
-    <h1>Développeur web - web mobile full stack</h1>
+        <h1>Développeur web - web mobile full stack</h1>
 
+    </div>
     <div class="bloc">
 
         <h2>Profil</h2>
         <p>
 
         <ul>
-            <li>En formation de développeur web / web-mobil full stack chez Simplon.</li>
-            <li>53 rue Ferdinand Buisson 62200 boulogne sur mer</li>
-            <li>0766591470</li>
+            <li>En formation de développeur web / web-mobil full stack chez Simplon.</li><br>
+            <li>53 rue Ferdinand Buisson 62200 boulogne sur mer</li><br>
+            <li>0766591470</li><br>
             <li>philippe.bourgeois62@laposte.net</li>
         </ul>
         </p>
 
     </div>
+    <div class="bloc">
+        <h2>Formations</h2>
+        <p>
+
+        <ul>
+            <li><strong>2010: </strong>CAP de Menisier agenceur</li><br>            
+            <li><strong>2008: </strong>CAP d'Ebeniste</li><br>
+            <li><strong>2005: </strong>BAC scientifique option maths</li><br>
+
+        </ul>
+        </p>
+    </div>
 
     <div class="bloc">
 
-        <h2>Competences</h2>
+        <h2>Savoir-être</h2>
         <p>
 
         <ul><strong>
-                <li>Autonomie</li>
-                <li>Capacité de décision</li>
-                <li>Curiosité</li>
-                <li>Créactivité</li>
-                <li>Rigueur</li>
-                <li>Travail en équipe</li>
+                <li>Autonomie</li><br>
+                <li>Capacité de décision</li><br>
+                <li>Curiosité</li><br>
+                <li>Créactivité</li><br>
+                <li>Rigueur</li><br>
+                <li>Travail en équipe</li><br>
         </ul></strong>
         </p>
 
     </div>
+
+    <div class="bloc">
+        <h2>Experiences</h2>
+        <p>
+            Dernière expérience (02/2020 - 05/2022) : <strong>Agent d'entretien des espaces naturels à Rivages propres.</strong><br><br> Experience m'ayant permis de travailler en complète autonomie et parfois avec la responsabilité d'une équipe.
+        </p>
+    </div>   
 
     <div class="bloc">
 
@@ -53,37 +74,23 @@
 
         <ul>
             <li><strong>Anglais </strong>Intermédiaire</li>
-            <li><strong>Français </strong>Courant</li>
-            <li><strong>Permis: </strong>B- Véhicule léger</li>
-            <li><strong>Centre d'interet: </strong>Ebenisterie, musique, jardinage</li>
+            <li><strong>Français </strong>Courant</li><br>
+            <li><strong>Permis: </strong>B- Véhicule léger</li><br>
+            <li><strong>Centres d'interet: </strong>Ebenisterie, musique, jardinage</li>
         </ul>
         </p>
 
     </div>
-
     <div class="bloc">
-        <h2>Formations</h2>
+        <h2>Reseaux</h2>
         <p>
 
         <ul>
-            <li><strong>2010: </strong>CAP de Menisier agenceur</li>
-            <li><strong>2008: </strong>CAP d'Ebeniste</li>
-            <li><strong>2005: </strong>BAC scientifique option maths</li>
-
+            <li><a href="https://www.linkedin.com/in/philippe-bourgeois-76662723b/">linkedin</a></li>
+            <li><a href="https://github.com/Pbourgeois62">Github</a></li>
         </ul>
         </p>
     </div>
-
-    <div class="bloc">
-        <h2>Experiences</h2>
-        <p>
-            Dernière expérience(02/2020 - 05/2022) : <strong>Agent d'entretien des espaces naturels à Rivages propres.</strong> Experience m'ayant permis de travailler en complète autonomie et parfois avec la responsabilité d'une équipe.
-        </p>
-    </div>
-
-
-
-
 </body>
 
 </html
