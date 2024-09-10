29230b632d0ec81c2226c52d62ee4519f72f5dc2
Pbourgeois62 <pbourgeois62200@gmail.com>
2024-09-10 13:27:09 +0200

    Premier commit

---
+++ cv.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>CV</title>
+</head>
+<body>
+
+<h1>Développeur web -web mobile full stack</h1>  
+</body>
+</html>
