6cf8fc9f5a724b267e4ac6fa7cab51cd9dac3570
Pbourgeois62 <pbourgeois62200@gmail.com>
2024-09-10 14:00:23 +0200

    Deuxieme commit

--- cv.php
+++ cv.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,6 +8,6 @@
 </head>
 <body>
 
-<h1>Développeur web -web mobile full stack</h1>  
+<h1>Développeur web - web mobile full stack</h1>  
 </body>
-</html>
+</html
