2c9f790badcb9722d5055dcbf20cf7935d9dafbb
Pbourgeois62 <pbourgeois62200@gmail.com>
2024-09-11 15:53:02 +0200

    Pbs de CSSGrid

--- cv.php
+++ cv.php
@@ -9,87 +9,79 @@
 </head>
 
 <body>
-    <div class="bloc">
+    <div class="conteneur-grid">
+        <div class="bloc" id="blocPhoto">
+            <img src="docs/" alt="Photo de profil" width="" height="">
+            <h1>Développeur web - web mobile full stack</h1>
+        </div>
+        <div class="bloc" id="blocProfil">
+            <h2>Profil</h2>
+            <p>
+            <ul>
+                <li>En formation de développeur web / web-mobil full stack chez Simplon.</li><br>
+                <li>53 rue Ferdinand Buisson 62200 boulogne sur mer</li><br>
+                <li>0766591470</li><br>
+                <li>philippe.bourgeois62@laposte.net</li>
+            </ul>
+            </p>
+        </div>
 
-        <img src="docs/" alt="Photo de profil" width="" height="">
 
-        <h1>Développeur web - web mobile full stack</h1>
-
-    </div>
-    <div class="bloc">
-
-        <h2>Profil</h2>
-        <p>
-
-        <ul>
-            <li>En formation de développeur web / web-mobil full stack chez Simplon.</li><br>
-            <li>53 rue Ferdinand Buisson 62200 boulogne sur mer</li><br>
-            <li>0766591470</li><br>
-            <li>philippe.bourgeois62@laposte.net</li>
-        </ul>
-        </p>
-
-    </div>
-    <div class="bloc">
-        <h2>Formations</h2>
-        <p>
-
-        <ul>
-            <li><strong>2010: </strong>CAP de Menisier agenceur</li><br>            
-            <li><strong>2008: </strong>CAP d'Ebeniste</li><br>
-            <li><strong>2005: </strong>BAC scientifique option maths</li><br>
-
-        </ul>
-        </p>
-    </div>
-
-    <div class="bloc">
-
-        <h2>Savoir-être</h2>
-        <p>
-
-        <ul><strong>
+        <div class="bloc" id="blocFormations">
+            <h2>Formations</h2>
+            <p>
+            <ul>
+                <li><strong>2010: </strong>CAP de Menisier agenceur</li><br>
+                <li><strong>2008: </strong>CAP d'Ebeniste</li><br>
+                <li><strong>2005: </strong>BAC scientifique option maths</li><br>
+            </ul>
+            </p>
+        </div>
+        <div class="bloc" id="blocCompetences">
+            <h2>Savoir-être</h2>
+            <p>
+            <ul>
                 <li>Autonomie</li><br>
                 <li>Capacité de décision</li><br>
                 <li>Curiosité</li><br>
                 <li>Créactivité</li><br>
                 <li>Rigueur</li><br>
                 <li>Travail en équipe</li><br>
-        </ul></strong>
-        </p>
-
-    </div>
-
-    <div class="bloc">
-        <h2>Experiences</h2>
-        <p>
-            Dernière expérience (02/2020 - 05/2022) : <strong>Agent d'entretien des espaces naturels à Rivages propres.</strong><br><br> Experience m'ayant permis de travailler en complète autonomie et parfois avec la responsabilité d'une équipe.
-        </p>
-    </div>   
-
-    <div class="bloc">
-
-        <h2>Divers</h2>
-        <p>
-
-        <ul>
-            <li><strong>Anglais </strong>Intermédiaire</li>
-            <li><strong>Français </strong>Courant</li><br>
-            <li><strong>Permis: </strong>B- Véhicule léger</li><br>
-            <li><strong>Centres d'interet: </strong>Ebenisterie, musique, jardinage</li>
-        </ul>
-        </p>
-
-    </div>
-    <div class="bloc">
-        <h2>Reseaux</h2>
-        <p>
-
-        <ul>
-            <li><a href="https://www.linkedin.com/in/philippe-bourgeois-76662723b/">linkedin</a></li>
-            <li><a href="https://github.com/Pbourgeois62">Github</a></li>
-        </ul>
-        </p>
+            </ul>
+            </p>
+        </div>
+        <div class="bloc" id="blockExperiences">
+            <h2>Experiences</h2>
+            <p>
+                <li>Dernière expérience (02/2020 - 05/2022) : <strong>Agent d'entretien des espaces naturels à Rivages propres.</strong><br><br> Experience m'ayant permis de travailler en complète autonomie et parfois avec la responsabilité d'une équipe.</li>
+            </p>
+        </div>
+        <div class="bloc" id="blocDivers">
+            <h2>Divers</h2>
+            <p>
+            <ul>
+                <li><strong>Anglais </strong>Intermédiaire</li>
+                <li><strong>Français </strong>Courant</li><br>
+                <li><strong>Permis: </strong>B - Véhicule léger</li><br>
+                <li><strong>Centres d'interet: </strong><br><br>
+                    <ul>
+                        <li>Ebenisterie</li>
+                        <li>Musique</li>
+                        <li>Jardinage</li>
+                    </ul>
+                </li>
+            </ul>
+            </p>
+        </div>
+        <div class="bloc" id="blocReseaux">
+            <h2>Reseaux</h2>
+            <p>
+            <ul>
+                <li><a href="https://www.linkedin.com/in/philippe-bourgeois-76662723b/" target="blank">linkedin</a></li>
+                <li><a href="https://github.com/Pbourgeois62" target="blank">Github</a></li>
+            </ul>
+            </p>
+        </div>
     </div>
 </body>
 
