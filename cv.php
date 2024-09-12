4d924f9c9d925f812f92c0768c16d5b04820803c
Pbourgeois62 <pbourgeois62200@gmail.com>
2024-09-12 20:35:04 +0200

    Nouvelle version de CSSGrid

--- cv.php
+++ cv.php
@@ -1,64 +1,40 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CVFlex</title>
     <link rel="stylesheet" href="styles.css">
-    <title>CV</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 
 <body>
-    <div class="conteneur-grid">
-        <div class="bloc" id="blocPhoto">
-            <img src="docs/" alt="Photo de profil" width="" height="">
+    <div id="top"></div>
+    <div class="container">
+        <div class="Photo bloc">
+            <img src="docs/singe.jpg" alt="Photo de profil" width="" height="">
             <h1>Développeur web - web mobile full stack</h1>
         </div>
-        <div class="bloc" id="blocProfil">
+        <div class="Profil bloc">
             <h2>Profil</h2>
-            <p>
             <ul>
-                <li>En formation de développeur web / web-mobil full stack chez Simplon.</li><br>
-                <li>53 rue Ferdinand Buisson 62200 boulogne sur mer</li><br>
-                <li>0766591470</li><br>
-                <li>philippe.bourgeois62@laposte.net</li>
+                <li>
+                    <i class="fa-solid fa-book fa-beat"></i><br>En formation de développeur web / web-mobil full stack chez Simplon.
+                </li><br>
+                <li>
+                    <i class="fa-solid fa-house fa-beat"></i><br> 53 rue Ferdinand Buisson 62200 boulogne sur mer
+                </li><br>
+                <li>
+                    <i class="fa-solid fa-phone fa-beat"></i><br>0766591470
+                </li><br>
+                <li>
+                    <i class="fa-solid fa-envelope fa-beat"></i><br>philippe.bourgeois62@laposte.net
+                </li>
             </ul>
-            </p>
         </div>
-
-
-        <div class="bloc" id="blocFormations">
-            <h2>Formations</h2>
-            <p>
-            <ul>
-                <li><strong>2010: </strong>CAP de Menisier agenceur</li><br>
-                <li><strong>2008: </strong>CAP d'Ebeniste</li><br>
-                <li><strong>2005: </strong>BAC scientifique option maths</li><br>
-            </ul>
-            </p>
-        </div>
-        <div class="bloc" id="blocCompetences">
-            <h2>Savoir-être</h2>
-            <p>
-            <ul>
-                <li>Autonomie</li><br>
-                <li>Capacité de décision</li><br>
-                <li>Curiosité</li><br>
-                <li>Créactivité</li><br>
-                <li>Rigueur</li><br>
-                <li>Travail en équipe</li><br>
-            </ul>
-            </p>
-        </div>
-        <div class="bloc" id="blockExperiences">
-            <h2>Experiences</h2>
-            <p>
-                <li>Dernière expérience (02/2020 - 05/2022) : <strong>Agent d'entretien des espaces naturels à Rivages propres.</strong><br><br> Experience m'ayant permis de travailler en complète autonomie et parfois avec la responsabilité d'une équipe.</li>
-            </p>
-        </div>
-        <div class="bloc" id="blocDivers">
+        <div class="Divers bloc">
             <h2>Divers</h2>
-            <p>
             <ul>
                 <li><strong>Anglais </strong>Intermédiaire</li>
                 <li><strong>Français </strong>Courant</li><br>
@@ -71,18 +47,48 @@
                     </ul>
                 </li>
             </ul>
-            </p>
         </div>
-        <div class="bloc" id="blocReseaux">
+        <div class="Reseaux bloc ">
             <h2>Reseaux</h2>
-            <p>
-            <ul>
-                <li><a href="https://www.linkedin.com/in/philippe-bourgeois-76662723b/" target="blank">linkedin</a></li>
-                <li><a href="https://github.com/Pbourgeois62" target="blank">Github</a></li>
+            <ul id="ul-reseaux">
+                <li><a href="https://www.linkedin.com/in/philippe-bourgeois-76662723b/" target="blank" class="bouton">
+                    <i class="fa-brands fa-linkedin"></i></a></li>
+                <li><a href="https://github.com/Pbourgeois62" target="blank" class="bouton">
+                    <i class="fa-brands fa-github"></i></a></li>
             </ul>
-            </p>
         </div>
+        <div class="Competences bloc ">
+            <h2>Compétences</h2>
+            <ul>
+                <li>Autonomie</li><br>
+                <li>Capacité de décision</li><br>
+                <li>Curiosité</li><br>
+                <li>Créactivité</li><br>
+                <li>Rigueur</li><br>
+                <li>Travail en équipe</li><br>
+            </ul>
+        </div>
+        <div class="Experiences bloc ">
+            <h2>Experiences</h2>
+            <ul>
+                <li>Dernière expérience (02/2020 - 05/2022) : <strong>Agent d'entretien des espaces naturels à Rivages propres.</strong><br><br>
+                    Experience m'ayant permis de travailler en complète autonomie et parfois avec la responsabilité d'une équipe.</li>
+            </ul>
+        </div>
+        <div class="Formations bloc ">
+            <h2>Formations</h2>
+            <ul>
+                <ul>
+                    <li><strong>2010: </strong>CAP de Menisier agenceur</li><br>
+                    <li><strong>2008: </strong>CAP d'Ebeniste</li><br>
+                    <li><strong>2005: </strong>BAC scientifique option maths</li><br>
+                </ul>
+            </ul>
+        </div>
+    </div>
+    <div class="bouton-ancre">
+        <a href="#top"><i class="fa-solid fa-chevron-up"></i></a>
     </div>
 </body>
 
-</html
+</html>
